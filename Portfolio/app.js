9e00c955aa8eb3ece18e6f85280e7a789452e4e9
krilian88 <72553918+krilian88@users.noreply.github.com>
2021-04-13 23:29:18 +0300

    Add files via upload

---
+++ Portfolio/app.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobileMenu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header.container');
+const menuItem = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    let scrollPosition = window.scrollY;
+    if (scrollPosition > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+menuItem.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
+    });
+});
